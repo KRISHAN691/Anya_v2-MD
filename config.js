969d4997402bb1629c9d3041b2e672cdea5cd1dd
KRISHAN691 <149302500+KRISHAN691@users.noreply.github.com>
2023-11-07 20:57:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,18 +9,18 @@ global.zApiKey = {// if this API key get expired then please go to https://www.f
 
 
 global.botname = process.env.BotName || "Queen Anya";
-global.author = process.env.Author || "@PikaBotz";
+global.author = process.env.Author || "@KRISHAN";
 global.packname = process.env.PackName || "Queen Anya v2 MD";
 global.myweb = process.env.Web || "https://github.com/PikaBotz";
 global.footer = process.env.Footer || "© Queen Anya Bot";
 global.prefa = process.env.Prefix || ['-']
 global.themeemoji = process.env.ThemeEmoji || "🎐";
-global.ownername = process.env.Owner_Name || "Pika~Kun";
-global.ownernumber = process.env.Owner_Number || "916900904828";
-global.adress = process.env.Continent || "Asia, India, Assam";
-global.timezone = process.env.TimeZone || "Asia/Kolkata";
+global.ownername = process.env.Owner_Name || "KRISHANx";
+global.ownernumber = process.env.Owner_Number || "94764224187";
+global.adress = process.env.Continent || "Asia, India, Srilanka";
+global.timezone = process.env.TimeZone || "Asia/colombo";
 global.instagramId = process.env.Insta || "8.08_only_mine";
-global.email = process.env.Email_Id || "example@example.com";
+global.email = process.env.Email_Id || "krishanxsl@gmail.com";
 
 global.mongoUrl = process.env.MongoDB || "Enter"; 
 
@@ -32,10 +32,10 @@ global.left = process.env.Left_Msg || 'Ex-member *@$user* is no longer available
 global.promote = process.env.Promote_Msg || '*@$user* has been promoted as an admin in this group.\n\n_$prefix promotem off to disable this message._';
 global.demote = process.env.Demote_Msg || '*@$user* has been demoted to a member in this group.\n\n_$prefix demotem off to disable this message._';
 
-global.sessionId = process.env.SESSION_ID || "GeneticsP_AN_YA_rotests";
+global.sessionId = process.env.SESSION_ID || "Houn_AN_YA_dAn_AN_YA_tique";
 global.auto_read_status  = process.env.AUTO_READ_STATUS || "true";
 global.alwaysonline = process.env.ALWAYS_ONLINE || "true";
-global.readmessage = process.env.READ_MESSAGE || "true";
+global.readmessage = process.env.READ_MESSAGE || "false";
 global.auto_status_saver = process.env.AUTO_STATUS_SAVER || "true";
 
 global.badWords = [
